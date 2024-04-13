ded68fd39297035aa3d7217a73e7bf323f78d3b0
Sarobidy <ratsaraefadahysarobidy@gmail.com>
2024-04-13 21:16:16 +0300

    configuration

---
+++ src/main/java/mg/itu/tpbanquesarobidy/config/ConfigJSF.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mg.itu.tpbanquesarobidy.config;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.annotation.FacesConfig;
+
+/**
+ * Configuration JSF
+ * @author Sarobidy
+ */
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+    
+}
