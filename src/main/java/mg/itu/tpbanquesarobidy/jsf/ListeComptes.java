772ae236540e700e11a4f27d57451bca9fed358c
Sarobidy <ratsaraefadahysarobidy@gmail.com>
2024-04-14 17:20:29 +0300

    ajout du backing bean ListeComptes

---
+++ src/main/java/mg/itu/tpbanquesarobidy/jsf/ListeComptes.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package mg.itu.tpbanquesarobidy.jsf;
+
+import jakarta.inject.Named;
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Inject;
+import java.io.Serializable;
+import java.util.List;
+import mg.itu.tpbanquesarobidy.entity.CompteBancaire;
+import mg.itu.tpbanquesarobidy.service.GestionnaireCompte;
+
+/**
+ *
+ * @author Sarobidy
+ */
+@Named(value = "listeComptes")
+@ViewScoped
+public class ListeComptes implements Serializable {
+
+    private List<CompteBancaire> allComptes;
+
+    @Inject
+    private GestionnaireCompte gestionnaireCompte;
+
+    /**
+     * Creates a new instance of ListeComptes
+     */
+    public ListeComptes() {
+    }
+
+    public List<CompteBancaire> getAllComptes() {
+        if (allComptes == null) {
+            allComptes = gestionnaireCompte.getAllComptes();
+        }
+        return allComptes;
+    }
+}
